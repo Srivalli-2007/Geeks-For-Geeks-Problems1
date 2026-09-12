c563272ded28af23ba0a307af223d6bf4759c00d
Srivalli-2007 <sreevallinalli@gmail.com>
2026-09-12 21:39:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Word Lengths/word-lengths.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    ArrayList<Integer> wordLengths(String s) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        String[] words = s.split(" ");
+        for (String word : words) {
+            ans.add(word.length());
+        }
+
+        return ans;
+    }
+}
