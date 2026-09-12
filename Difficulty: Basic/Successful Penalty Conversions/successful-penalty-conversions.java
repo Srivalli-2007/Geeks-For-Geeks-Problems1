53a6eda28cf6e5dac6787d9e8ea51bfeca1f8265
Srivalli-2007 <sreevallinalli@gmail.com>
2026-09-12 22:13:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Successful Penalty Conversions/successful-penalty-conversions.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCoversions(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '2' && s.charAt(i + 1) == '1')
+                count++;
+        }
+        return count;
+    }
+}
