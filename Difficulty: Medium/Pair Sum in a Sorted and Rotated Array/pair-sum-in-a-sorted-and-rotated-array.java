6091a1395c68db8a40e65396eebce947920255f5
Srivalli-2007 <sreevallinalli@gmail.com>
2026-09-19 21:33:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pair Sum in a Sorted and Rotated Array/pair-sum-in-a-sorted-and-rotated-array.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    static boolean pairInSortedRotated(int arr[], int target) {
+        // Your code here
+        int n=arr.length;
+        Arrays.sort(arr);
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int s=arr[left]+arr[right];
+            if(s<target){
+                left++;
+            }
+            else if(s>target){
+                right--;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
